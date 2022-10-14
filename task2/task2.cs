e80359c3278657211493339739e57f69de095217
Tatiana Yakovleva <mellyorchid@gmail.com>
2022-10-14 14:40:39 +0300

    first commit

---
+++ task2/task2.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+
+Console.Write("Введите координаты: ");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double D = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+
+Console.WriteLine(Math.Round(D, 2));
---
+++ task3/task3.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= N) {
+    Console.Write(Math.Pow(i, 3));
+    Console.Write(" ");
+    i++;
+}
